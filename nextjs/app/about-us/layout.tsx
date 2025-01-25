b8fe21bdcf1a4a4b25d8d7c517cba2d7a4e3706a
JonghwaLee <misterlee95@gmail.com>
2025-01-25 23:30:52 +0900

    0125 TIL

--- nextjs/app/about-us/layout.tsx
+++ nextjs/app/about-us/layout.tsx
@@ -1,16 +1,10 @@
+import Navigation from "../components/navigation";
 
-export default function AboutUsLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function AboutUsLayout({ children }: { children: React.ReactNode }) {
   return (
     <div>
-
       {children}
       &copy; Next JS is great!
-
     </div>
-
-  )
+  );
 }
--- nextjs/app/about-us/page.tsx
+++ nextjs/app/about-us/page.tsx
@@ -1,5 +1,13 @@
-import Navigation from "../components/navigation"
+import Navigation from "../components/navigation";
+
+export const metadata = {
+  title: "About us",
+};
 
 export default function AboutUs() {
-  return (<div><h1>About us!</h1></div>)
+  return (
+    <div>
+      <h1>About us!</h1>
+    </div>
+  );
 }
