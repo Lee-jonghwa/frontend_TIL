96b9b08ac71ce7cf14bbd51eb6ae4c25d0ad05c3
JonghwaLee <misterlee95@gmail.com>
2025-01-27 13:32:03 +0900

    0127 TIL

--- nextjs/app/(movies)/movies/[id]/page.tsx
+++ nextjs/app/(movies)/movies/[id]/page.tsx
@@ -1,22 +1,21 @@
-import { API_URL } from "../../../(home)/page";
-
-async function getMovie(id: string) {
-  console.log(`Fetching movies: ${Date.now()}`);
-  const response = await fetch(`${API_URL}/${id}`);
-  return response.json();
-}
-
-async function getVideos(id: string) {
-  console.log(`Fetching videos: ${Date.now()}`);
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-  const response = await fetch(`${API_URL}/${id}/videos`);
-  return response.json();
-}
-
+import { Suspense } from "react";
+import MovieInfo from "../../../components/movie-info";
+import MovieVideos from "../../../components/movie-videos";
 export default async function MovieDetail({ params: { id } }: { params: { id: string } }) {
   await new Promise((resolve) => setTimeout(resolve, 1000));
-  console.log("start fetching");
-  const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
-  console.log("end fetching");
-  return <h1> {movie.title} </h1>;
+  return (
+    <div>
+      <h3>Movie Detail Page</h3>
+      <Suspense fallback={<h1>Loading movie info</h1>}>
+        {/* movie info와 함께 보임 */}
+        <h3>movie info</h3>
+        <MovieInfo id={id} />
+      </Suspense>
+      {/* 처음 렌더링 될때 바로 보임 */}
+      <h3>movie videos</h3>
+      <Suspense fallback={<h1>Loading vidoes</h1>}>
+        <MovieVideos id={id} />
+      </Suspense>
+    </div>
+  );
 }
